b89be114a77254285f2e89b7d341ef7031659d10
EduardoAyora <eduardoaayora64@gmail.com>
2020-10-07 23:17:58 -0500

    footer y about us

--- footer.php
+++ footer.php
@@ -16,6 +16,10 @@
 				<div class="col-sm-6 col-md-4">
 					<h3>IMPORPRIMA</h3>
 					<p>Encuentra productos en Cuenca a buen precio</p>
+					<h3>Redes sociales</h3>
+					<a href="https://www.facebook.com/Imporprima-115183470323611/" target="_blank">
+						<i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+					</a>
 				</div>
 				<div class="col-sm-6 col-md-4">
 					<h3>Secciones</h3>
@@ -28,10 +32,14 @@
 					</ul>
 				</div>
 				<div class="col-sm-6 col-md-4">
-					<h3>Redes sociales</h3>
-					<a href="https://facebook.com" target="_blank">
-						<i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
-					</a>
+					<h3>Contactos:</h3>
+					<ul>
+						<li><a href = "mailto: imporprima4@gmail.com">imporprima4@gmail.com</a></li>
+						<li>Maria José Ochoa: 0997122275</li>
+						<li>Tamara Salamea: 0981273552</li>
+						<li>Valeria Ochoa: 0985334377</li>
+						<li>Jenny Salamea: 0996321890</li>
+					</ul>
 				</div>
 			</div>
 		</div>
